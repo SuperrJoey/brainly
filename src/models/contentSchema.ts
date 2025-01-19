1802312b2ff7183bd628857f610e44ff77f1d349
SuperrJoey <devdaryaan@gmail.com>
2025-01-19 23:54:26 +0530

    useless commit

--- src/models/contentSchema.ts
+++ src/models/contentSchema.ts
@@ -8,7 +8,8 @@ const ContentSchema = new Schema({
     link:  { type: String, required: true },
     type: { type: String, enum: contentTypes, required: true },
     tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
-    userId: {type: mongoose.Types.ObjectId, ref: 'User'}
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true,}
 })
 
 export const contentModel = model("Content", ContentSchema);
+ 
